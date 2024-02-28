705ba1ecbc07b9f1a51c7903288370fde4c4a889
islom2202 <islomihsan@gmail.com>
2024-02-28 17:58:09 +0500

    added footer in all languages

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -5,13 +5,18 @@ export const Footer = () => {
   const icons:string[] = t("footer.softclub.icons", { returnObjects: true })
   const informationList:string[] = t("footer.information.list", { returnObjects: true}) 
   const contactList:{icon:string, name:string}[] = t("footer.contact.list", { returnObjects: true })
+  const copyright:[] = t("footer.copyright", { returnObjects: true})
   return (
     <>
       <footer className="footer">
         <div className="footer__softclub">
-          <img src={getIconUrl(t("footer.softclub.image"))} alt="logo" />
+          <img
+            src={getIconUrl(t("footer.softclub.image"))}
+            alt="logo"
+            className="footer__softclub__logo"
+          />
           <p>{t("footer.softclub.description")}</p>
-          <ul>
+          <ul className="footer__softclub__icons">
             {icons.map((e) => (
               <li key={e}>
                 <img src={getIconUrl(e)} alt={e} />
@@ -41,7 +46,13 @@ export const Footer = () => {
           </ul>
         </section>
       </footer>
-      <footer className="copyright"></footer>
+      <footer className="copyright">
+        <div>
+          {copyright.map((e) => (
+            <li>{e}</li>
+          ))}
+        </div>
+      </footer>
     </>
   )
 }
